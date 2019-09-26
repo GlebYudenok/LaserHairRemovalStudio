2d0ee9452bba947436bef2881a59a717a297ba23
user <gyudenok@mail.ru>
2019-09-26 15:19:33 +0300

    create database n tables structure

---
+++ sql/createDatabase.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE IF NOT EXISTS laserhairremovalstudiodb DEFAULT CHAR SET UTF8;
---
+++ sql/createTables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS USER (
+                    id VARCHAR(255) NOT NULL ,
+                    login VARCHAR(255) NOT NULL ,
+                    password VARCHAR(255) NOT NULL ,
+                    email VARCHAR(255) NOT NULL ,
+                    role ENUM('Admin', 'Master', 'Client') NOT NULL ,
+                    PRIMARY KEY (id)
+                    );
+
+CREATE TABLE IF NOT EXISTS SERVICE (
+                        id VARCHAR(255),
+                        zone_name VARCHAR(255),
+                        price DECIMAL,
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE IF NOT EXISTS COMPLEX_SERVICE (
+                        id VARCHAR(255),
+                        service_id VARCHAR(255),
+                        FOREIGN KEY (service_id) REFERENCES SERVICE(id),
+                        PRIMARY KEY (id,service_id)
+                        );
+
+CREATE TABLE IF NOT EXISTS APPOINTMENT (
+                             date_n_time DATETIME NOT NULL,
+                             user_id VARCHAR(255),
+                             service_id VARCHAR(255),
+                             complex_id VARCHAR(255),
+                             foreign key (complex_id) REFERENCES COMPLEX_SERVICE(id),
+                             foreign key (user_id) REFERENCES USER(id),
+                             foreign key (service_id) REFERENCES SERVICE(id),
+                             primary key (user_id)
+                         );
+
+CREATE TABLE IF NOT EXISTS USER_INFO (
+                        id VARCHAR(255) ,
+                        name VARCHAR(255) NOT NULL ,
+                        surname VARCHAR(255) NOT NULL ,
+                        avatar_link TEXT NOT NULL ,
+                        birth_date VARCHAR(255) NOT NULL ,
+                        phone_number VARCHAR(255) NOT NULL,
+                        FOREIGN KEY (id) REFERENCES USER(id),
+                        PRIMARY KEY (id)
+                        );
+
+CREATE TABLE IF NOT EXISTS PICTURES (
+                        id VARCHAR(255),
+                        link TEXT,
+                        PRIMARY KEY (id)
+);
