569b6270f75eb3158e0cd8a8b2147757da5fef7f
user <gyudenok@mail.ru>
2019-10-04 14:36:47 +0300

    set right order for delete tables

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,8 +1,7 @@
-DELETE FROM complex_service;
-DELETE FROM service;
-DELETE FROM user_info;
-DELETE FROM user;
 DELETE FROM user_info;
 DELETE FROM appointment;
+DELETE FROM complex_service;
+DELETE FROM service;
+DELETE FROM user;
 
 
