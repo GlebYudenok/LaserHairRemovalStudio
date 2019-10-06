9f0312c68016ac84b2540d183b12c289fe5f4e1e
user <gyudenok@mail.ru>
2019-10-06 21:17:52 +0300

    add on delete cascade feature

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS USER (
-                    id VARCHAR(255) NOT NULL ,
+                    id VARCHAR(255) ,
                     login VARCHAR(255) NOT NULL UNIQUE ,
                     password VARCHAR(255) NOT NULL ,
                     email VARCHAR(255) NOT NULL ,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS USER_INFO (
                                      birth_date DATE NOT NULL ,
                                      phone_number VARCHAR(255) NOT NULL ,
                                      gender ENUM ('male', 'female', 'other') ,
-                                     FOREIGN KEY (user_id) REFERENCES USER(id),
+                                     FOREIGN KEY (user_id) REFERENCES USER(id) ON DELETE CASCADE,
                                      PRIMARY KEY (user_id)
 );
 
@@ -33,19 +33,19 @@ CREATE TABLE IF NOT EXISTS COMPLEX_SERVICE (
                         price DECIMAL,
                         name VARCHAR(255),
                         user_gender enum ('male', 'female', 'other'),
-                        FOREIGN KEY (service_id) REFERENCES SERVICE(id),
+                        FOREIGN KEY (service_id) REFERENCES SERVICE(id) ON DELETE CASCADE ,
                         PRIMARY KEY (id,service_id)
                         );
 
 CREATE TABLE IF NOT EXISTS APPOINTMENT (
                              id VARCHAR(255) NOT NULL ,
                              date_n_time TIMESTAMP NOT NULL UNIQUE ,
-                             user_id VARCHAR(255),
+                             user_id VARCHAR(255) ,
                              service_id VARCHAR(255),
                              complex_id VARCHAR(255),
-                             foreign key (complex_id) REFERENCES COMPLEX_SERVICE(id),
-                             foreign key (user_id) REFERENCES USER(id),
-                             foreign key (service_id) REFERENCES SERVICE(id),
+                             foreign key (complex_id) REFERENCES COMPLEX_SERVICE(id) ON DELETE CASCADE ,
+                             foreign key (user_id) REFERENCES USER(id) ON DELETE CASCADE ,
+                             foreign key (service_id) REFERENCES SERVICE(id) ON DELETE CASCADE ,
                              primary key (id)
                          );
 
