8f311cf4d01c6036dd05a89518545c0282f403cb
user <gyudenok@mail.ru>
2019-10-07 00:27:19 +0300

    add pictureDao interface

--- src/main/java/by/gyudenok/dao/PictureDao.java
+++ src/main/java/by/gyudenok/dao/PictureDao.java
@@ -2,8 +2,9 @@ package by.gyudenok.dao;
 
 import by.gyudenok.entity.Picture;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface PictureDao extends Dao<Picture> {
-    List<Picture> readAll();
+    List<Picture> readAll() throws SQLException;
 }
