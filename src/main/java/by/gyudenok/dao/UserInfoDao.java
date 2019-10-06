e168e56723cbdf7be37d605f211769e10983e0f7
user <gyudenok@mail.ru>
2019-10-07 00:57:12 +0300

    add UserInfoDao interface

--- src/main/java/by/gyudenok/dao/UserInfoDao.java
+++ src/main/java/by/gyudenok/dao/UserInfoDao.java
@@ -2,9 +2,10 @@ package by.gyudenok.dao;
 
 import by.gyudenok.entity.UserInfo;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface UserInfoDao extends Dao<UserInfo> {
-    List<UserInfo> readAll();
+    List<UserInfo> readAll() throws SQLException;
     UserInfo readByName(String name, String surname);
 }
