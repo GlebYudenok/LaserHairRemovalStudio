9df435eeb6d68f454a35cfe6a9504cb0d73effc2
user <gyudenok@mail.ru>
2019-10-07 00:26:54 +0300

    add serviceDao interface

--- src/main/java/by/gyudenok/dao/ServiceDao.java
+++ src/main/java/by/gyudenok/dao/ServiceDao.java
@@ -2,9 +2,10 @@ package by.gyudenok.dao;
 
 import by.gyudenok.entity.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ServiceDao extends Dao<Service> {
-    List<Service> readAll();
+    List<Service> readAll() throws SQLException;
     Service readByName(String name);
 }
