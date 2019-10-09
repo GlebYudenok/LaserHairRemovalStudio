22dd7361db2ce601d1fc724a5f868ed226dd8c8a
user <gyudenok@mail.ru>
2019-10-09 20:46:00 +0300

    add picture service

--- src/main/java/by/gyudenok/service/PictureService.java
+++ src/main/java/by/gyudenok/service/PictureService.java
@@ -1,4 +1,8 @@
 package by.gyudenok.service;
 
+import by.gyudenok.exception.ServiceException;
+
 public interface PictureService<Picture> {
+    boolean addPictureToGalery(Picture picture) throws ServiceException;
+    boolean deletePicture(String id) throws ServiceException;
 }
--- src/main/java/by/gyudenok/service/impl/PictureServiceImpl.java
+++ src/main/java/by/gyudenok/service/impl/PictureServiceImpl.java
@@ -1,6 +1,44 @@
 package by.gyudenok.service.impl;
 
+import by.gyudenok.dao.PictureDao;
+import by.gyudenok.dao.factory.SqlDaoFactory;
+import by.gyudenok.entity.Picture;
+import by.gyudenok.exception.DaoException;
+import by.gyudenok.exception.ServiceException;
+import by.gyudenok.exception.ValidatorException;
 import by.gyudenok.service.PictureService;
+import by.gyudenok.service.validator.PictureValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class PictureServiceImpl implements PictureService {
+public class PictureServiceImpl implements PictureService<Picture> {
+
+    private static final Logger LOGGER = LogManager.getLogger(PictureServiceImpl.class);
+    private static final SqlDaoFactory factory = SqlDaoFactory.getInstance();
+    private static final PictureDao<Picture> pictureDao = factory.getSqlPictureDao();
+    private static final PictureValidator validator = new PictureValidator();
+
+    @Override
+    public boolean addPictureToGalery(Picture picture) throws ServiceException {
+        try {
+            validator.validateNull(picture.getLink());
+            validator.validateNull(picture.getId());
+            pictureDao.create(picture);
+        }catch (ValidatorException | DaoException e) {
+            throw new ServiceException(e.getMessage());
+        }
+        LOGGER.info("picture was added successfully");
+        return true;
+    }
+
+    @Override
+    public boolean deletePicture(String id) throws ServiceException {
+        try {
+            validator.validateNull(id);
+            pictureDao.delete(id);
+        }catch (ValidatorException | DaoException e) {
+            throw new ServiceException(e.getMessage());
+        }
+        return true;
+    }
 }
