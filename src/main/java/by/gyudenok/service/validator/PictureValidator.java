4d17cf957269e584fb0ffe48f44a3f49887a02b0
user <gyudenok@mail.ru>
2019-10-09 21:22:22 +0300

    add some methods for picture validating

--- src/main/java/by/gyudenok/service/validator/PictureValidator.java
+++ src/main/java/by/gyudenok/service/validator/PictureValidator.java
@@ -1,11 +1,19 @@
 package by.gyudenok.service.validator;
 
+import by.gyudenok.entity.Picture;
 import by.gyudenok.exception.ValidatorException;
 
 public class PictureValidator {
     public boolean validateNull(String variable) throws ValidatorException {
         if(variable == null) {
             throw new ValidatorException("Variable is null");
+        }
+        return true;
+    }
+
+    public boolean validateNullPicture(Picture picture) throws ValidatorException {
+        if(picture == null) {
+            throw new ValidatorException("Argument is null");
         }
         return true;
     }
