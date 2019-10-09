8dd89d2a77535f76ad2ae6c5b1ef4bd264a567de
user <gyudenok@mail.ru>
2019-10-09 20:09:58 +0300

    add generic interface

--- src/main/java/by/gyudenok/service/UserService.java
+++ src/main/java/by/gyudenok/service/UserService.java
@@ -1,7 +1,6 @@
 package by.gyudenok.service;
 
 import by.gyudenok.entity.UserInfo;
-import by.gyudenok.exception.DaoException;
 import by.gyudenok.exception.ServiceException;
 
 import java.util.List;
@@ -9,8 +8,8 @@ import java.util.List;
 public interface UserService<User> {
     User signIn(String login, String password) throws ServiceException;
     User signUp(User user, UserInfo userInfo) throws ServiceException;
-    User findById(String id);
+    User findById(String id) throws ServiceException;
     boolean deleteAccount(String id) throws ServiceException;
-    List<User> getListOfUsers();
-    boolean editProfile(User user, UserInfo userInfo);
+    List<User> getListOfUsers() throws ServiceException;
+    boolean editProfile(User user, UserInfo userInfo) throws ServiceException;
 }
