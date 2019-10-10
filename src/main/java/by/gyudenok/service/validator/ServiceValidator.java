57e4d1697eaeb23c9daa8ae2bbac346448d162df
user <gyudenok@mail.ru>
2019-10-10 17:07:35 +0300

    replaced by service validator for service layer

--- src/main/java/by/gyudenok/service/validator/ServiceValidator.java
+++ src/main/java/by/gyudenok/service/validator/ServiceValidator.java
@@ -1,19 +1,11 @@
 package by.gyudenok.service.validator;
 
-import by.gyudenok.entity.Service;
 import by.gyudenok.exception.ValidatorException;
 
-public class ServiceValidator {
-    public boolean checkServiceNull(Service service) throws ValidatorException {
-        if(service == null) {
+public class ServiceValidator<T> {
+    public boolean checkNull(T obj) throws ValidatorException {
+        if(obj == null) {
             throw new ValidatorException("Argument is null");
-        }
-        return true;
-    }
-
-    public boolean checkIdNull(String id) throws ValidatorException {
-        if(id == null) {
-            throw new ValidatorException("Id is null");
         }
         return true;
     }
